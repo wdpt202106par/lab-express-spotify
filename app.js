ceb9a9484e228ac6f651497367422e7d3bce4819
massytak <75225037+massytak@users.noreply.github.com>
2021-04-27 19:47:25 +0200

    4 iteration

--- app.js
+++ app.js
@@ -13,7 +13,38 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+
 // setting the spotify-api goes here:
+
+app.get('/',function(req,res,next){
+    res.render('home')
+})
+
+app.get('/artist-search',function(req,res,next){
+    spotifyApi
+  .searchArtists(`${req.query.artist}`)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    res.render('artist-search-results',{
+       artists : data.body.artists.items  
+    } )
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+})
 
 // Our routes go here:
 
